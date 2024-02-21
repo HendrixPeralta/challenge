f3a5b2aa5651a20979903a19e8239afc8d81c275
hendrixperalta <hendrixperalta@gmail.com>
2024-02-21 22:14:21 +0900

    Added parameter result_carry
    Carries the result in a list to the next iteration

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
 # init
 
-ls = [1, 3, 5, 5, 10]
+ls = [1, -3, 5, 5, 10]
 counter = 0
 suma = 0
 
 
-def sq_sum(lst, i, total):
-    if i == 0:
+def sq_sum(lst, i=None, total=None, result_carry=None):
+    if i is None:
+        i = 0
         total = 0
+        result_carry = []
 
+    # Delete before sending *************
     print("i: " + str(i))
     print("number: " + str(lst[i]))
+
+    # checks if the number in the list is negative
+    if lst[i] < 0:
+        print("im here")
+        i += 1
+        sq_sum(lst, i, total, result_carry)
+
     result = lst[i] ** 2 + total
     i += 1
     print("result: " + str(result))
 
     if i >= len(lst):
-        result = 0
+        print("*" * 40)
         print("total is: " + str(result))
-        return result
 
-    sq_sum(lst, i, result)
+        result_carry.append(result)
+        print(result_carry)
+        return result_carry
+
+    sq_sum(lst, i, result, result_carry)
 
 
-sq_sum(ls, counter, suma)
+def show_result(lst, i=None):
+    print(lst[i])
+    if lst[i] >= len(lst):
+        return
+    i += 1
+    show_result(lst, i)
+
+
+sq_sum(ls)
