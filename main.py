e51e4c3b2d0cd5280d5e90d2f21174705ae3f0ff
hendrixperalta <hendrixperalta@gmail.com>
2024-02-21 21:38:03 +0900

    Added sq_sum()
    Receives a list of strings and returns the sum of squares without loops

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-#init
+# init
+
+ls = [1, 3, 5, 5, 10]
+counter = 0
+suma = 0
+
+
+def sq_sum(lst, i, total):
+    if i == 0:
+        total = 0
+
+    print("i: " + str(i))
+    print("number: " + str(lst[i]))
+    result = lst[i] ** 2 + total
+    i += 1
+
+    if i >= len(lst):
+        result = 0
+        print("total is: " + str(result))
+        return result
+
+    sq_sum(lst, i, result)
+
+
+sq_sum(ls, counter, suma)
