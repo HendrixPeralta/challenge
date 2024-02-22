e06049b235e0bb744329229a56bf59ac38d5a8df
hendrixperalta <hendrixperalta@gmail.com>
2024-02-22 23:21:25 +0900

    Eliminated sq_sum()
    fill_num() square the numbers before adding them to the array and compute() sums the array

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ def show_result(num_list, i=0):
 
 
 # Must pass a list.
-def sq_sum(num_list, i=0, n=None, result_carry=None):
-    if n == 0:
-        return sum(result_carry)
-
-    if result_carry is None:
-        result_carry = []
-        n = len(num_list)
-
-    result_carry.append(num_list[i] ** 2)
-    return sq_sum(num_list, i+1, n-1, result_carry)
+# def sq_sum(num_list, i=0, n=None, result_carry=None):
+#     if n == 0:
+#         return sum(result_carry)
+#
+#     if result_carry is None:
+#         result_carry = []
+#         n = len(num_list)
+#
+#     result_carry.append(num_list[i] ** 2)
+#     return sq_sum(num_list, i+1, n-1, result_carry)
 
 # Do not know if its possible to use lambda
 # def sq_sum(num_lst):
@@ -24,10 +24,11 @@ def sq_sum(num_list, i=0, n=None, result_carry=None):
 
 
 # Negative numbers are converted to 0 to comply with the challenge rules
+# Creates a list with the squared input
 def fill_num(num_list, n):
     if n == 0:
         return
-    num_list.append(max(0, int(input())))
+    num_list.append(max(0, int(input()))**2)
     fill_num(num_list, n-1)
 
 
@@ -38,7 +39,7 @@ def compute(cycle, result):
     num = max(0, int(input()))
     num_list = []
     fill_num(num_list, num)
-    result.append(sq_sum(num_list))
+    result.append(sum(num_list))
     compute(cycle-1, result=result)
 
 
