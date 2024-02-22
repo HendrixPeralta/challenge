f616ac03bdddd814f3b7d2d9818e2ec7ae458e9e
hendrixperalta <hendrixperalta@gmail.com>
2024-02-22 23:25:06 +0900

    Eliminated sq_sum()
    fill_num() square the numbers before adding them to the array and compute() sums the array

--- main.py
+++ main.py
@@ -6,23 +6,6 @@ def show_result(num_list, i=0):
     show_result(num_list, i+1)
 
 
-# Must pass a list.
-# def sq_sum(num_list, i=0, n=None, result_carry=None):
-#     if n == 0:
-#         return sum(result_carry)
-#
-#     if result_carry is None:
-#         result_carry = []
-#         n = len(num_list)
-#
-#     result_carry.append(num_list[i] ** 2)
-#     return sq_sum(num_list, i+1, n-1, result_carry)
-
-# Do not know if its possible to use lambda
-# def sq_sum(num_lst):
-#     return sum(list(map(lambda x: x**2, num_lst)))
-
-
 # Negative numbers are converted to 0 to comply with the challenge rules
 # Creates a list with the squared input
 def fill_num(num_list, n):
