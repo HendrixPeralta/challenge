e0497d78788a61c46bac26516d75f5f2ca41434a
hendrixperalta <hendrixperalta@gmail.com>
2024-02-27 00:47:34 +0900

    Modified sq_sum()
    Now it iterates to find the sum of squares and returns an int value with the result

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ def show_result(result, i=0):
     show_result(result, i+1)
 
 
-def sq_sum(num_char, result):
-    clean_list = map(lambda x: max(0, int(x)), num_char.split(" "))
-    result.append(sum(list(map(lambda x: int(x)**2, clean_list))))
 # num_list = A list of integer numbers (str/int).
 # n = the length of the list (int)
 # result_carry = empty list
+def sq_sum(num_list, i=0, n=None, result_carry=None):
+    if n == 0:
         # returns an int value result of the sum of squares
+        return sum(result_carry)
     # Converts the negative values into 0 to avoid counting them in the final result then it squares the number.
+    result_carry.append(max(0, int(num_list[i]))**2)
+    return sq_sum(num_list, i+1, n-1, result_carry)
 
 
 # cycle = the amount of times we want to repeat the process (int).
@@ -36,5 +38,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
