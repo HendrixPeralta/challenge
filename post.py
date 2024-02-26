3abf98e73bbd007ad13c1dc217714e12eba8f484
hendrixperalta <hendrixperalta@gmail.com>
2024-02-26 17:00:34 +0900

    Added body
    contains the data to send to the server

---
+++ post.py
@@ -0,0 +1,11 @@
+import requests
+
+
+body = {
+  "github_url": "https://gist.github.com/HendrixPeralta/64f9d69f5b3b5361ef92ed0917ea26d6",
+  "contact_email": "hendrixperalta@gmail.com",
+  "solution_language": "python"
+}
+
+
+# https://api.challenge.hennge.com/challenges/003
